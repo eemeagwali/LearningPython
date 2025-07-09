bd34229da528c2b9a3450f45f8f65187928b1915
eemeagwali <ifeonline2001@yahoo.com>
2025-07-08 22:25:27 -0700

    Python commit 1

---
+++ Exercise.py
@@ -0,0 +1,17 @@
+# ▶️ 1. Creating Variables and Assigning Values
+name = "Emmanuel Emeagwali"
+age = 35
+height = 1.75  # in meters
+is_student = False
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is student:", is_student)
+
+# ✅ Task 1: Create variables for your city (string), number of kids (int), and has_pet (boolean)
+city = "Vancouver"
+num_kids = 2
+has_pet = True
+print("Your city:", city)
+print("Number of kids:", num_kids)
+print("Has pet:", has_pet)
